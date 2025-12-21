e9553d0b86871f389895fc674fea7d11294800ed
xiangliujing1014-dev <xiangliujing1014@gmail.com>
2025-12-21 20:48:27 +0800

    change

--- app/src/main/java/android/team08_memorygame/MemoryAdapter.kt
+++ app/src/main/java/android/team08_memorygame/MemoryAdapter.kt
@@ -7,10 +7,10 @@ import android.widget.AbsListView
 import android.widget.BaseAdapter
 import android.widget.ImageView
 
-// 1. 数据模型
+
 data class Card(val imageId: Int, var isFaceUp: Boolean = false, var isMatched: Boolean = false)
 
-// 2. 适配器
+
 class MemoryAdapter(private val context: Context, private val cards: List<Card>) : BaseAdapter() {
 
     override fun getCount(): Int {
@@ -29,7 +29,7 @@ class MemoryAdapter(private val context: Context, private val cards: List<Card>)
         val imageView: ImageView
         if (convertView == null) {
             imageView = ImageView(context)
-            // 设置卡片大小：300x300像素
+            //resize
             imageView.layoutParams = AbsListView.LayoutParams(300, 300)
             imageView.scaleType = ImageView.ScaleType.CENTER_CROP
             imageView.setPadding(8, 8, 8, 8)
@@ -39,11 +39,11 @@ class MemoryAdapter(private val context: Context, private val cards: List<Card>)
 
         val card = cards[position]
 
-        // 显示图片逻辑
+        // display pictures
         if (card.isFaceUp || card.isMatched) {
             imageView.setImageResource(card.imageId)
         } else {
-            // 背面：设置为空图片，显示灰色背景
+            // if imageview=null  set gray
             imageView.setImageResource(0)
             imageView.setBackgroundColor(context.getColor(android.R.color.darker_gray))
         }
